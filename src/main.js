7813012ab3038c8ce582bca43d73b18eeb7f748f
ArtemGoiT <150847319+ArtemGoiT@users.noreply.github.com>
2024-03-29 15:51:22 +0100

    sss

---
+++ src/main.js
@@ -0,0 +1,36 @@
+const timerBlock = document.querySelector('.timer-time');
+const deadline = '29 Mar 2024';
+
+let interval;
+
+const updateClock = () => {
+  const date = new Date().getTime();
+  const dateDeadline = new Date(deadline).getTime();
+  const timeRemaining = (dateDeadline - date) / 1000;
+
+  const days = Math.floor(timeRemaining / 60 / 60 / 24);
+  const hours = Math.floor((timeRemaining / 60 / 60) % 24);
+  const minutes = Math.floor((timeRemaining / 60) % 60);
+
+  timerBlock.innerHTML = `
+    <div class="timer-count">${days}<span>дни</span></div>
+    <div class="timer-sep">:</div>
+    <div class="timer-count">${('' + hours).padStart(
+      2,
+      '0'
+    )}<span>часы</span></div>
+    <div class="timer-sep">:</div>
+    <div class="timer-count">${('' + minutes).padStart(
+      2,
+      '0'
+    )}<span>минуты</span></div>`;
+
+  if (timeRemaining <= 0) {
+    clearInterval(interval);
+    timerBlock.textContent = `акция закончилась`;
+  }
+};
+
+updateClock();
+
+interval = setInterval(updateClock, 500);
