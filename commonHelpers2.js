a9f14c849856d3fd0d751f84775e42210d184446
ArtemGoiT <150847319+ArtemGoiT@users.noreply.github.com>
2024-03-29 14:58:11 +0000

    Deploying to gh-pages from @ ArtemGoiT/Hamala@e3be0b26a7a16b4d28fbba3664e1d77486ebb829 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,5 +1,5 @@
 (function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&i(n)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();const c=document.querySelector(".timer-time"),l="14 April 2024";let a;const d=()=>{const s=new Date().getTime(),r=(new Date(l).getTime()-s)/1e3,i=Math.floor(r/60/60/24),e=Math.floor(r/60/60%24),t=Math.floor(r/60%60);c.innerHTML=`
-    <div class="timer-count">${i}<span>дни</span></div>
+    <div class="timer-count">${i}<span>дней</span></div>
     <div class="timer-sep">:</div>
     <div class="timer-count">${(""+e).padStart(2,"0")}<span>часы</span></div>
     <div class="timer-sep">:</div>
